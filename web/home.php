9f90a8962de0ee104f2f8b7c6cc0d6734fba8084
rpot <ryanpotocnik@yahoo.com>
2016-06-22 16:59:47 -0400

    database 2 add

--- web/home.php
+++ web/home.php
@@ -47,6 +47,20 @@ $db = pg_connect( "$host $port $dbname $credentials"  );
    } else {
       echo "Opened database successfully\n";
    }
+pg_close($db);
+   
+echo "Opening database 2\n";
+$host = "host=ec2-54-235-177-62.compute-1.amazonaws.com";
+$port= "port=5432";
+$dbname = "dbname=dfif9883lkmv0m";
+$credentials = "user=ecyxybbwrnfpyt password=HAtdGLA-NZz5JTBddqmFFXSarD";
+$db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+pg_close($db);
 
 ?>
 
